6c4360ef6d38695f2fc382b9a6c2bb7f998c5ae2
Robert Bishop <rbishop85@gmail.com>
2022-10-16 21:33:09 -0700

    got basic page load order and nav setup, currently unstyled but following example of activity 24

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume Page</h1>
+      <p>
+        This is a default Resume page.
+      </p>
+    </div>
+  );
+}
