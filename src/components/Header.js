4a45ed1ea49f29ec506e5355d774fc56a530cbe1
Robert Bishop <rbishop85@gmail.com>
2022-12-05 21:20:53 -0800

    changed styling for header component to go through central styles.css file

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,11 @@ import { Grid } from '@mui/material';
 import React from 'react';
 import Navigation from './Navigation';
 
-// In-line styling
-const styles = {
-    header: {
-      overflow: 'auto',
-      paddingTop: 25,
-      paddingBottom: 25,
-      borderBottom: "3px solid #007b6d"
-    },
-    title: {
-      marginTop: 0,
-      marginBottom: 0
-    }
-  };
-
 export default function Header({ currentTab, setCurrentTab }) {
   return (
-    <Grid container spacing={2} style={styles.header}>
+    <Grid container spacing={2} className="header">
       <Grid item xs={12} md={3}>
-        <h1 style={styles.title}>Robert Bishop</h1>
+        <h1 className="title">Robert Bishop</h1>
       </Grid>
       <Grid item xs={12} md={6}>
         {/* Load navigation component */}
