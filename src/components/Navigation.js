d67674e903f719e8ddabf111847d226cb4ab8d56
Robert Bishop <rbishop85@gmail.com>
2022-12-05 22:05:51 -0800

    changed styling for navigation component to go through central styles.css file

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,34 +1,7 @@
 import * as React from 'react';
 import { Link } from "react-router-dom";
 
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import { styled } from "@mui/material/styles";
-
-// In-line styling
-const styles = {
-  navigation: {
-    marginTop: 0
-  }
-};
-
-// Custom styling for MUI tabs
-const StyledTab = styled(Tab)({
-  "&.MuiTouchRipple-root": {
-    color: "#D3D3D3"
-  },
-  "&.MuiButtonBase-root.MuiTab-root": {
-    fontSize: "15px",
-    fontWeight: 700,
-    paddingLeft: 0,
-    paddingRight: 0
-  },
-  "&.Mui-selected": {
-    color: "#D3D3D3",
-    fontSize: "15px"
-  },
-})
+import { Tabs, Tab, Box } from '@mui/material';
 
 function Navigation({ currentTab, setCurrentTab }) {
 
@@ -40,37 +13,34 @@ function Navigation({ currentTab, setCurrentTab }) {
   const routes = ["/", "/portfolio", "/contact", "resume"]
 
   return (
-    <Box sx={{ width: '100%', }} style={styles.navigation}>
+    <Box className="navigation">
       <Tabs
         value={currentTab}
         onChange={handleChange}
         centered
         textColor="inherit"
-        sx={{
-          '& .MuiTabs-indicator': { backgroundColor: "#D3D3D3" }
-        }}
         aria-label="navigation tabs"
       >
         {/* Individual tabs for pages */}
-        <StyledTab 
+        <Tab 
           value="about" 
           label="About"
           component={Link}
           to={routes[0]}
         />
-        <StyledTab 
+        <Tab 
           value="portfolio" 
           label="Portfolio"
           component={Link}
           to={routes[1]}
         />
-        <StyledTab 
+        <Tab 
           value="contact" 
           label="Contact"
           component={Link}
           to={routes[2]}
         />
-        <StyledTab 
+        <Tab 
           value="resume" 
           label="Resume"
           component={Link}
