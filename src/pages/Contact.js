efae73c771cfee39aa388a94e6c41fc46bc6b9e6
Robert Bishop <rbishop85@gmail.com>
2022-12-06 22:45:03 -0800

    moved contact page styling to styles.css file

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,21 +3,6 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 
-// In-line styling
-const styles = {
-  title: {
-    textAlign: "center",
-  },
-};
-
-const formStyle = {
-  border: `2px solid`,
-  borderRadius: "4px",
-  padding: 12,
-  backgroundColor: "#00000090",
-};
-
-
 const Contact = ({ setCurrentTab }) => {
 
   // Ensures that if site is loaded directly to this page that the proper tab will bet highlighted
@@ -37,36 +22,13 @@ const Contact = ({ setCurrentTab }) => {
   };
 
   return (
-    <Grid container justifyContent="center">
+    <Grid container justifyContent="center" className="contact">
       <Grid item xs={10} md={6}>
-        <h2 style={styles.title}>Contact Me:</h2>
-        <form style={formStyle} onSubmit={handleSubmit}>
+        <h2 className="title">Contact Me:</h2>
+        <form className="form" onSubmit={handleSubmit} autoComplete="off">
           <Grid container direction="column" alignItems="center">
             <TextField
               required
-              // Manually adjusting colors for MUI text box field
-              sx={{
-                "& .MuiInputBase-root": {
-                  color: "#D3D3D3",
-                },
-                "& .MuiFormLabel-root": {
-                  color: "#007b6d",
-                },
-                "& .MuiFormLabel-root.Mui-focused": {
-                  color: "#007b6d",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: "#007b6d",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "#007b6d",
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#007b6d",
-                  },
-                },
-              }}
               id="contactName"
               label="Name"
               variant="outlined"
@@ -77,29 +39,6 @@ const Contact = ({ setCurrentTab }) => {
             />
             <TextField
               required
-              // Manually adjusting colors for MUI text box field
-              sx={{
-                "& .MuiInputBase-root": {
-                  color: "#D3D3D3",
-                },
-                "& .MuiFormLabel-root": {
-                  color: "#007b6d",
-                },
-                "& .MuiFormLabel-root.Mui-focused": {
-                  color: "#007b6d",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: "#007b6d",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "#007b6d",
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#007b6d",
-                  },
-                },
-              }}
               id="contactEmail"
               label="E-Mail"
               variant="outlined"
@@ -110,29 +49,6 @@ const Contact = ({ setCurrentTab }) => {
             />
             <TextField
               required
-              sx={{
-                // Manually adjusting colors for MUI text box field
-                "& .MuiInputBase-root": {
-                  color: "#D3D3D3",
-                },
-                "& .MuiFormLabel-root": {
-                  color: "#007b6d",
-                },
-                "& .MuiFormLabel-root.Mui-focused": {
-                  color: "#007b6d",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: "#007b6d",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "#007b6d",
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#007b6d",
-                  },
-                },
-              }}
               id="contactMessage"
               label="Message"
               variant="outlined"
