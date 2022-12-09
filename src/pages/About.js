80a4abc5a4484a94a25e87bee14120dd376aab43
Robert Bishop <rbishop85@gmail.com>
2022-12-08 21:40:37 -0800

    implimented static header and footers with scrolling content in between, starting on resume page re-work

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Grid, Container } from '@mui/material';
 // Pulling in npm for displaying my github calendar
 import GitHubCalendar from "react-github-calendar";
-// Pulling in icons for my list of tools
-import { SiHtml5, SiCss3, SiJavascript, SiNodedotjs, SiExpress, SiReact, SiMysql, SiMongodb } from 'react-icons/si'
 
 import selfImage from '../assets/images/me.jpg'
 
@@ -15,72 +13,34 @@ export default function About({ setCurrentTab }) {
   return (
     <div className="about">
       <Container>
-      <Grid container direction="column" alignItems="center">
-        {/* Picture of me */}
-      <img src={selfImage} alt="Me in front of some trees" className="image"/>
-      
-      {/* Brief section talking about myself */}
-      <h2>About Me</h2>
-      <p>
-        Welcome to my portfolio. My name is Robert Bishop and I'm currently a student in a Web Coding Bootcamp being run by the University of Washington. I've had an interest in computers going back to when I started tinkering with my personal computer in high school. After high school I pursued an Associates program in computer networking through a tech college in Arizona before returning to my home in the PNW.
-      </p>
-      <p>
-        After college I held on to the interest but didn't really pursue IT work in a professional capacity. For the past 11 years I've been working for a local copy machine dealer handling both hardware and connectivity service calls. A few months back I started talking to a family member about this Coding Bootcamp, the more I read up on it the more I felt like it would be the perfect fit for me. I really enjoy working through the process of trying to take an idea that's in my mind and spending time creating it in code.
-      </p>
-      <p>
-        When I'm not in class or at work I spend most of my time with my wife and son. I also enjoy tinkering with the project car in the garage and occasionally attending concerts with my wife.
-      </p>
-      <p>
-        I'm looking forward to opportunities to both test and prove my capabilities, as well as simply learn new things.
-      </p>
-      <div className="divider" />
-      {/* Icons and descriptions for languages I'm trained in */}
-      <h2>My Tools</h2>
-      <Grid container justifyContents="center" xs={10} md={6}>
-        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
-          <SiHtml5 className="ToolIcon" />
-          HTML5
+        <Grid container direction="column" alignItems="center">
+          {/* Picture of me */}
+          <img src={selfImage} alt="Me in front of some trees" className="image"/>
+        
+          {/* Brief section talking about myself */}
+          <h2>About Me</h2>
+          <p>
+            Welcome to my portfolio. My name is Robert Bishop and I'm currently a student in a Web Coding Bootcamp being run by the University of Washington. I've had an interest in computers going back to when I started tinkering with my personal computer in high school. After high school I pursued an Associates program in computer networking through a tech college in Arizona before returning to my home in the PNW.
+          </p>
+          <p>
+            After college I held on to the interest but didn't really pursue IT work in a professional capacity. For the past 11 years I've been working for a local copy machine dealer handling both hardware and connectivity service calls. A few months back I started talking to a family member about this Coding Bootcamp, the more I read up on it the more I felt like it would be the perfect fit for me. I really enjoy working through the process of trying to take an idea that's in my mind and spending time creating it in code.
+          </p>
+          <p>
+            When I'm not in class or at work I spend most of my time with my wife and son. I also enjoy tinkering with the project car in the garage and occasionally attending concerts with my wife.
+          </p>
+          <p>
+            I'm looking forward to opportunities to both test and prove my capabilities, as well as simply learn new things.
+          </p>
+          <div className="divider" />
+          <h2>Coding Activity</h2>
+          <GitHubCalendar
+          // Configuration for github commit calendar display
+            username="rbishop85"
+            blockSize={15}
+            blockMargin={5}
+            fontSize={16}
+          />
         </Grid>
-        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
-          <SiCss3 className="ToolIcon" />
-          CSS3
-        </Grid>
-        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
-          <SiJavascript className="ToolIcon" />
-          Javascript
-        </Grid>
-        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
-          <SiNodedotjs className="ToolIcon" />
-          NodeJS
-        </Grid>
-        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
-          <SiExpress className="ToolIcon" />
-          Express
-        </Grid>
-        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
-          <SiReact className="ToolIcon" />
-          React
-        </Grid>
-        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
-          <SiMysql className="ToolIcon" />
-          MySQL
-        </Grid>
-        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
-          <SiMongodb className="ToolIcon" />
-          MongoDB
-        </Grid>
-
-      </Grid>
-      <div className="divider" />
-      <h2>Coding Activity</h2>
-      <GitHubCalendar
-      // Configuration for github commit calendar display
-        username="rbishop85"
-        blockSize={15}
-        blockMargin={5}
-        fontSize={16}
-      />
-      </Grid>
       </Container>
     </div>
   );
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
+import React from 'react';
 import { Grid, Container } from '@mui/material';
 import Button from '@mui/material/Button';
+
+// Pulling in icons for my list of tools
+import { SiHtml5, SiCss3, SiJavascript, SiNodedotjs, SiExpress, SiReact, SiMysql, SiMongodb } from 'react-icons/si'
 
 // Store pdf of resume in variable
 import pdf from '../assets/pdf/resume2.pdf';
@@ -11,57 +13,52 @@ export default function Resume({ setCurrentTab }) {
   // Ensures that if site is loaded directly to this page that the proper tab will bet highlighted
   setCurrentTab("resume")
 
-  // Set state for pdf page info
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
   return (
+    <div className="resume">
     <Container>
-    <Grid container direction="column" alignItems="center" className="resume">
-      <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess} xs={12}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      {/* If pdf has more than 1 page, load the buttons used to transition pages */}
-      {numPages > 1 &&
-        <Grid container justifyContent="center" className="pages">
-          <Button
-            variant="contained"
-            onClick={() => {
-              // If the page number is higher than one, set the page number 1 back
-              if(pageNumber > 1) {
-                setPageNumber(pageNumber - 1);
-              }
-            }}
-          >
-            Previous
-          </Button>
-          {/* Display current page number */}
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-          <Button
-            variant="contained"
-            onClick={() => {
-              // If the page number is lower than the max pages then set the page number 1 higher
-              if(pageNumber < numPages) {
-                setPageNumber(pageNumber + 1);
-              }
-              
-            }}
-          >
-            Next
-          </Button>
+    <Grid container direction="column" alignItems="center">
+      {/* Icons and descriptions for languages I'm trained in */}
+      <h2>My Tools</h2>
+      <Grid container justifyContents="center" xs={10} md={6}>
+        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
+          <SiHtml5 className="ToolIcon" />
+          HTML5
         </Grid>
-      }
+        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
+          <SiCss3 className="ToolIcon" />
+          CSS3
+        </Grid>
+        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
+          <SiJavascript className="ToolIcon" />
+          Javascript
+        </Grid>
+        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
+          <SiNodedotjs className="ToolIcon" />
+          NodeJS
+        </Grid>
+        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
+          <SiExpress className="ToolIcon" />
+          Express
+        </Grid>
+        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
+          <SiReact className="ToolIcon" />
+          React
+        </Grid>
+        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
+          <SiMysql className="ToolIcon" />
+          MySQL
+        </Grid>
+        <Grid container direction="column" alignItems="center" justifyContent="space-between" xs={3} className="tool">
+          <SiMongodb className="ToolIcon" />
+          MongoDB
+        </Grid>
+      </Grid>
+      <p>Education:</p>
+      <p>Work History:</p>
       {/* Button to download copy of resume */}
-      <Button variant="outlined" href={pdf} download>Download</Button>
+      <Button variant="outlined" href={pdf} download>Download Resume</Button>
     </Grid>
     </Container>
+    </div>
   );
 }
